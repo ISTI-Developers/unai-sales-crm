515a57828907f506d92f0afb88751938173a8a69
ISTI-Developer Kyle <isti@unmg.com.ph>
2025-11-24 11:08:20 +0800

    Remove rollup-plugin-visualizer from Vite configuration to streamline build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,11 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { visualizer } from "rollup-plugin-visualizer";
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ["**/*.xlsx"],
   plugins: [
     react(),
-    visualizer({
-      open: false, // Automatically opens the report
-      gzipSize: true, // Show gzip sizes
-      brotliSize: true, // Show brotli sizes
-      filename: "stats.html",
-    }),
   ],
   server: {
     port: 9424,
