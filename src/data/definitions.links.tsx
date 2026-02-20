c05efdabb583c27b886ce71884bd7fb8f5b472f6
ISTI-Developer Kyle <isti@unmg.com.ph>
2026-02-20 12:54:24 +0800

    feat: update sidebar components and enhance user data display with new permissions and layout adjustments

--- src/data/definitions.links.tsx
+++ src/data/definitions.links.tsx
@@ -14,6 +14,11 @@ import {
   UsersRound,
   Gpu,
   NotepadText,
+  TrainFront,
+  Plane,
+  Bus,
+  Signature,
+  PackageCheck,
 } from "lucide-react";
 import { lazy } from "react";
 
@@ -24,6 +29,9 @@ const Deck = lazy(() => import("@/views/Deck"));
 const Booking = lazy(() => import("@/views/Booking"));
 const Reports = lazy(() => import("@/views/Reports"));
 const Meetings = lazy(() => import("@/views/Meetings"));
+const LRT = lazy(() => import("@/views/LRT"));
+const Availability = lazy(() => import("@/views/Availability"));
+const Contracts = lazy(() => import("@/views/UTASIContracts"));
 const Companies = lazy(() => import("@/views/Companies"));
 const Mediums = lazy(() => import("@/views/Mediums"));
 const Users = lazy(() => import("@/views/Users"));
@@ -44,17 +52,22 @@ export interface LinkDefinitions {
 
 export const linkDefinitions: LinkDefinitions[] = [
   { title: "dashboard", handler: "", icon: LayoutDashboard, group: "general", element: Dashboard, },
-  { title: "sites", handler: "/sites", icon: Monitor, group: 'general', element: Sites, },
-  { title: "clients", handler: "/clients", icon: BookUser, group: 'general', element: Clients, },
+  { title: "sites", handler: "/sites", icon: Monitor, group: 'asset', element: Sites, },
+  { title: "clients", handler: "/clients", icon: BookUser, group: 'clients', element: Clients, },
   { title: "deck", handler: "/deck", icon: Gpu, group: 'general', element: Deck, },
   { title: "booking", handler: "/booking", icon: Tags, group: 'general', element: Booking, },
-  { title: "reports", handler: "/reports", icon: FileChartColumnIncreasing, group: 'general', element: Reports, },
-  { title: "meetings", handler: "/meetings", icon: NotepadText, group: 'general', element: Meetings, },
-  { title: "companies", handler: "/companies", icon: Building, group: 'manage', element: Companies, },
-  { title: "mediums", handler: "/mediums", icon: Package2, group: 'manage', element: Mediums, },
-  { title: "users", handler: "/users", icon: UsersRound, group: 'manage', element: Users },
-  { title: "roles", handler: "/roles", icon: UserRoundCog, group: 'manage', element: Roles },
-  { title: "logs", handler: "/logs", icon: History, group: 'manage', element: Logs },
-  { title: "help", handler: "/help", icon: CircleHelp, group: 'general', element: Help },
-  { title: "settings", handler: "/settings", icon: SettingsIcon, group: 'general', element: Settings, },
+  { title: "reports", handler: "/reports", icon: FileChartColumnIncreasing, group: 'clients', element: Reports, },
+  { title: "meetings", handler: "/meetings", icon: NotepadText, group: 'clients', element: Meetings, },
+  { title: "LRT", handler: "/lrt", icon: TrainFront, group: 'asset', element: LRT, },
+  { title: "MCIA", handler: "/mcia", icon: Plane, group: 'asset', element: Meetings, },
+  { title: "PITX", handler: "/pitx", icon: Bus, group: 'asset', element: Meetings, },
+  { title: "UTASI Contracts", handler: "/utasi_contracts", icon: Signature, group: 'general', element: Contracts, },
+  { title: "asset availability", handler: "/asset_availability", icon: PackageCheck, group: 'general', element: Availability, },
+  { title: "companies", handler: "/companies", icon: Building, group: 'admin', element: Companies, },
+  { title: "mediums", handler: "/mediums", icon: Package2, group: 'admin', element: Mediums, },
+  { title: "users", handler: "/users", icon: UsersRound, group: 'admin', element: Users },
+  { title: "roles", handler: "/roles", icon: UserRoundCog, group: 'admin', element: Roles },
+  { title: "logs", handler: "/logs", icon: History, group: 'admin', element: Logs },
+  { title: "help", handler: "/help", icon: CircleHelp, group: 'admin', element: Help },
+  { title: "settings", handler: "/settings", icon: SettingsIcon, group: 'admin', element: Settings, },
 ];
