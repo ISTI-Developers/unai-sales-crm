0ddbd1ed976cf84e95b9253a9a7f750269817f48
ISTI-Developer Kyle <isti@unmg.com.ph>
2026-03-09 14:51:37 +0800

    feat: Comment out Booking link and update changes for version 2.5.1

--- src/misc/changes.ts
+++ src/misc/changes.ts
@@ -1,7 +1,6 @@
 export const changes = {
-  "2.5.1": [
-    "Added the link definitions and the helper functions",
-  ],
+  "2.5.2": ["Updated bookings"],
+  "2.5.1": ["Added the link definitions and the helper functions"],
   "2.5": [
     "Revamped the site availability and booking",
     "Fixed the rendering of end dates",
