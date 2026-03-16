be96148677d3f2a5bbe55c67fb29ae5e7b6d59e5
ISTI-Developer Kyle <isti@unmg.com.ph>
2026-03-16 12:23:57 +0800

    feat: Add changelog entry for Meetings page revamp in version 2.6

--- src/misc/changes.ts
+++ src/misc/changes.ts
@@ -1,4 +1,5 @@
 export const changes = {
+  "2.6":["Meetings page revamp."],
   "2.5.5": ["Fixed a bug where the input field for populations are displayed as price."],
   "2.5.4": ["Fixed an issue where pre-termination contracts don't override the previous contract."],
   "2.5.3": ["Fixed the display of the booking graph in dashboard.","Fixed the booking restrictions."],
