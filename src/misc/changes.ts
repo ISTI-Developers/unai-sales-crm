03199699f1a865cdc607239cc0accfb9213ca83a
ISTI-Developer Kyle <isti@unmg.com.ph>
2026-05-04 11:43:28 +0800

    2.7.2 patch notes

--- src/misc/changes.ts
+++ src/misc/changes.ts
@@ -1,21 +1,5 @@
 export const changes = {
+  "2.7.2": ["Fixed an issue where deck images would disappear after generation."],
   "2.7.1": ["Optimized add client form."],
   "2.7": ["Updated Deck design", "Fixed booking availability.", "Fixed client adding issues"],
-  "2.6":["Meetings page revamp."],
-  "2.5.5": ["Fixed a bug where the input field for populations are displayed as price."],
-  "2.5.4": ["Fixed an issue where pre-termination contracts don't override the previous contract."],
-  "2.5.3": ["Fixed the display of the booking graph in dashboard.","Fixed the booking restrictions."],
-  "2.5.2": ["Updated bookings"],
-  "2.5.1": ["Added the link definitions and the helper functions"],
-  "2.5": [
-    "Revamped the site availability and booking",
-    "Fixed the rendering of end dates",
-    "Removed UNIS as booking contract source.",
-  ],
-  "2.4.2": ["fixed booking date issues on deck."],
-  "2.4.1": ["fixed booking date issues."],
-  "2.4": [
-    "updated booking options.",
-    "updated view of other bookings in dashboard.",
-  ],
 };
