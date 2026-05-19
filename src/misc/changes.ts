34728dc2646fe165e49fccbf5227f5b1b943ff47
ISTI-Developer Kyle <isti@unmg.com.ph>
2026-05-19 16:36:19 +0800

    fix: Add entry for version 2.7.3 with revamped booking feature

--- src/misc/changes.ts
+++ src/misc/changes.ts
@@ -1,4 +1,5 @@
 export const changes = {
+  "2.7.3": ["REVAMPED BOOKING :)"],
   "2.7.2": ["Fixed an issue where deck images would disappear after generation."],
   "2.7.1": ["Optimized add client form."],
   "2.7": ["Updated Deck design", "Fixed booking availability.", "Fixed client adding issues"],
