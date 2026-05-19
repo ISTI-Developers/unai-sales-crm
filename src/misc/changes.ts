2b35e6564b3a24ef00b0532d89eaff6adbb547fd
ISTI-Developer Kyle <isti@unmg.com.ph>
2026-05-19 17:57:19 +0800

    2.7.4 changes

--- src/misc/changes.ts
+++ src/misc/changes.ts
@@ -1,4 +1,5 @@
 export const changes = {
+  "2.7.4": ["Fixed role issues."],
   "2.7.3": ["REVAMPED BOOKING :)"],
   "2.7.2": ["Fixed an issue where deck images would disappear after generation."],
   "2.7.1": ["Optimized add client form."],
