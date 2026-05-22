99473649d10d7d90035506fb41d23e28cb2c8cc2
ISTI-Developer Kyle <isti@unmg.com.ph>
2026-05-22 14:42:34 +0800

    2.7.5 merge changes

--- src/misc/changes.ts
+++ src/misc/changes.ts
@@ -1,4 +1,5 @@
 export const changes = {
+  "2.7.5": ["Fixed booking issues."],
   "2.7.4": ["Fixed role issues."],
   "2.7.3": ["REVAMPED BOOKING :)"],
   "2.7.2": ["Fixed an issue where deck images would disappear after generation."],
