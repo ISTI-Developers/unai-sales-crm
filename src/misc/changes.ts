7e8a453a74a150be7c1eb06d41faad170e93d93a
ISTI-Developer Kyle <isti@unmg.com.ph>
2026-07-14 13:31:39 +0800

    merge changes

--- src/misc/changes.ts
+++ src/misc/changes.ts
@@ -1,13 +1,6 @@
 export const changes = {
-  "2.9.2": ['fixed missing clients for reports page'],
-  "2.9.1": ['Added under construction status'],
-  "2.9": ['Added batch transfer of ownership.'],
-  "2.8": ['Updated the look for client information.'],
-  "2.7.6": ["Allow searching of address and board facing"],
-  "2.7.5": ["Fixed booking issues."],
-  "2.7.4": ["Fixed role issues."],
-  "2.7.3": ["REVAMPED BOOKING :)"],
-  "2.7.2": ["Fixed an issue where deck images would disappear after generation."],
-  "2.7.1": ["Optimized add client form."],
-  "2.7": ["Updated Deck design", "Fixed booking availability.", "Fixed client adding issues"],
+  "2.9.3": ["Booking module changes."],
+  "2.9.2": ["fixed missing clients for reports page"],
+  "2.9.1": ["Added under construction status"],
+  "2.9": ["Added batch transfer of ownership."],
 };
