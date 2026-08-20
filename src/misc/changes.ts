e69bb6160c24d1c88ec4439ff52edaeae64d09d0
ISTI-Developer Kyle <isti@unmg.com.ph>
2026-08-20 17:36:00 +0800

    changelog updates

--- src/misc/changes.ts
+++ src/misc/changes.ts
@@ -1,4 +1,5 @@
 export const changes = {
+  "2.9.8.1": ["Updated the vite version for indexedDB"],
   "2.9.8": ["Optimized site images and rentals."],
   "2.9.7": ["Added discount capping to rate adjustments."],
   "2.9.6": ["New reports and meetings pages."],
